e359f3bdebd2302ddd38668af4bfbbae84305a78
Clint171 <clintsimiyu004@gmail.com>
2024-03-05 08:34:40 +0300

    feat: connected db and added routes

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {User , Apartment , Tenant , Feedback} = require("./schema/schema.js");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -16,6 +17,31 @@ let db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
     console.log("Connected to MongoDB");
+});
+
+app.get("db/:schema" , async (req , res)=>{
+    let schema = req.params.schema;
+    switch(schema){
+        case "User" : 
+            res.json(await User.find());
+            break;
+        case "Apartment" : 
+            res.json(await Apartment.find().populate("owner"));
+            break;
+        case "Tenant" :
+            res.json(await Tenant.find().populate("apartment"));
+            break;
+        case "Feedback" :
+            res.json(await Feedback.find().populate("user"));
+            break;
+        default : 
+            res.send("Unknown schema");
+    }
+    
+});
+
+app.use((req , res)=>{
+    res.send("An error occurred fetching the resource!");
 });
 
 app.listen(process.env.PORT, () => {
--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,4 +1,4 @@
-import mongoose from mongoose;
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -7,10 +7,59 @@ const userSchema = new mongoose.Schema({
         unique : true
     },
     password : String,
-
     email : String,
-    role : {
-        type : String,
-        enum : ["tenant" , "owner"]
-    }
+    role : String
 });
+
+const User = mongoose.model("User" , userSchema);
+
+const apartmentSchema = new mongoose.Schema({
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    address : String,
+    rooms : Number,
+    rent : Number,
+    occupiedRooms : Number
+});
+
+const Apartment = mongoose.model("Apartment" , apartmentSchema);
+
+const tenantSchema = new mongoose.Schema({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+        unique : true
+    },
+    apartment : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Apartment",
+        required : true
+    },
+    rent : Number,
+    paymentHistory : [Object]
+});
+
+const Tenant = mongoose.model("Tenant" , tenantSchema);
+
+const feedbackSchema = new mongoose.Schema({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    apartment : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Apartment",
+        required : true
+    },
+    text : String,
+    date : Date
+});
+
+const Feedback = mongoose.model("Feedback" , feedbackSchema);
+
+module.exports = { User , Apartment , Tenant , Feedback};
