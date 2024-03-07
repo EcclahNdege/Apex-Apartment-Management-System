84823190556bf0d026f860e4542df32b35f6e5d0
Clint171 <clintsimiyu004@gmail.com>
2024-03-07 15:37:21 +0300

    feat: added add-apartment

---
+++ frontend/resources/js/addApartment.js
@@ -0,0 +1,39 @@
+let apartmentForm = document.getElementById('apartment-form');
+
+apartmentForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let apartment = {
+        name: document.getElementById('name').value,
+        address: document.getElementById('address').value,
+        rooms: document.getElementById('rooms').value,
+        occupiedRooms: document.getElementById('occupied').value,
+        rent: document.getElementById('rent').value
+    }
+
+    fetch('https://apex-apartment-management-system.onrender.com/apartments/create', {
+        method: 'POST',
+        mode : "cors",
+        credentials : "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(apartment)
+    })
+        .then(response => {
+            if(response.status === 401) {
+                //window.location.href = 'login.html';
+                return;
+            }
+            else if(response.status === 403){
+                //window.location.href = 'login.html';
+                return;
+            }
+            else if(response.status === 400) {
+                alert('Invalid data');
+                throw new Error('Invalid data');
+            }
+            else{
+                window.location.href = 'apartments.html';
+            }})
+});
